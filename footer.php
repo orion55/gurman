b1b6195e21cda31b849d1a66dda83c36c3d5245b
Orion55 <grol55@mail.ru>
2018-04-23 14:39:32 +0500

    footer

--- footer.php
+++ footer.php
@@ -1,34 +1,24 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Gurman
- */
+</div><!-- #content -->
 
-?>
-
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'gurman' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'gurman' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'gurman' ), 'gurman', '<a href="http://portfolio.infoblog72.ru/">Grebenev O.E.</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<footer class="footer">
+    <div class="footer__wrap">
+        <div class="footer__phone">
+            <?php get_template_part('inc/phone'); ?>
+        </div>
+        <div class="footer__picture">
+            <a href="<?php echo home_url(); ?>" class="footer__link">
+                <div class="footer__pict hvr-grow"><img
+                            src="<?php echo get_template_directory_uri(); ?>/assets/img/header/logo.png" alt="logo"
+                            class="footer__img">
+                </div>
+            </a>
+        </div>
+        <div class="footer__social">
+            <?php get_template_part('inc/social'); ?>
+        </div>        
+    </div>
+</footer>
+</div>
 
 <?php wp_footer(); ?>
 
