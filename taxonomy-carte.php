473cf9e586b6c440dc9d50f3725645d8762cd3e7
Orion55 <grol55@mail.ru>
2018-04-25 00:15:13 +0500

    меню

--- taxonomy-carte.php
+++ taxonomy-carte.php
@@ -1,52 +1,53 @@
+<?php get_header(); ?>
+
+    <div class="dish">
+        <main class="dish__wrap">
+            <?php if (have_posts()) : ?>
+                <?php
+                while (have_posts()) :
+                    the_post();
+                    $title = get_the_title(); ?>
+
+                    <div class="course">
+                        <?php if (has_post_thumbnail()) : ?>
+                            <div class="course__pict">
+                                <img src="<?php the_post_thumbnail_url('medium'); ?>"
+                                     alt="<?php echo $title; ?>" class="course__img">
+                            </div>
+                        <?php endif; ?>
+                        <div class="course__block">
+                            <h2 class="course__title"><?php echo $title; ?></h2>
+                            <?php $content = get_the_content();
+                            if (!empty($content)):
+                                ?>
+                                <div class="course__desc">
+                                    <?php echo apply_filters('the_content', $content); ?>
+                                </div>
+                            <?php endif;
+                            $price = carbon_get_the_post_meta('price_dish');
+                            if (!empty($price)): ?>
+                                <div class="course__price">
+                                    <span class="course__text"><?php echo $price; ?></span>
+                                    <span class="course__text">руб.</span>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+
+                <?php endwhile; ?>
+
+                <?php if (function_exists('wp_pagenavi')) : ?>
+                    <div class="dish__pagenavi">
+                        <?php wp_pagenavi(); ?>
+                    </div>
+                <?php endif ?>
+
+            <?php else :
+                get_template_part('template-parts/content', 'none');
+            endif; ?>
+
+        </main>
+    </div>
+
 <?php
-/**
- * The template for displaying archive pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Gurman
- */
-
-get_header();
-?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
 get_footer();
